467b790c78b23402ea0148f877d75b0bd53c6358
hong pai <hongpai@hongmatoMacBook-Pro.local>
2012-06-20 15:09:22 +0800

    Initial Commit

---
+++ RabbitGame/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  RabbitGame
+//
+//  Created by pai hong on 12-6-20.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
